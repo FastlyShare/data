9c15be2bb1f7f3f91f24f318984f151b9fba1c62
sharefastly <mdya94812@outlook.com>
2025-05-23 06:26:53 +0530

    Add 26-06-23-05-2025_-_-ALL-folder_-_-12.c

---
+++ files/26-06-23-05-2025_-_-ALL-folder_-_-12.c
@@ -0,0 +1,16 @@
+#include < PC21xx.h>
+
+unsigned int delay;
+
+int main()
+
+{ 
+PINSELI = 0X00000000;  IODIR =0X00FF0000;
+White (1)
+{
+I00CLR = 0X00FF0000
+for (delay = 0; delay <500000;delay++);
+IOOSET = 0x00FF0000;
+for (delay = 0; delay<500000;deley++);
+}
+}
